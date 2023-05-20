03753a104838c7f3db579eb5f4326750d226d3ee
Paulo Henrique <paulo.henrique@hotmail.co.jp>
2023-05-20 17:55:34 -0300

    finish crud generic functions 2

--- src/econome/components/crud/Edit.vue
+++ src/econome/components/crud/Edit.vue
@@ -1,39 +1,62 @@
 <template>
   <Modal :slideOver="true" :show="show" @hidden="closeModal">
-      <ModalHeader class="p-5">
-          <h2 class="font-medium text-base mr-auto">
-              {{ info.title }}
-          </h2>
-      </ModalHeader>
-      <ModalBody>
-          <div v-for="(field, index) in info.fields" :key="index">
-              <div v-if="field.edit != false">
-                <label :for="`modal-form-${index}`" class="form-label">{{ field.title }}</label>
-                <template v-if="field.type === 'select'">
-                  <select :id="`modal-form-${index}`" class="form-control mb-4" v-model="form[field.model]">
-                      <option v-for="option in getOptions(field)" :key="option.id" :value="option.id">{{ option.name }}</option>
-                  </select>
-                </template>
-                <template v-else>
-                    <input :id="`modal-form-${index}`" :type="field.type" class="form-control mb-4" :placeholder="field.placeholder" v-model="form[field.model]" @keyup.enter="saveData" />
-                </template>
-              </div>
-          </div>
-      </ModalBody>
-      <ModalFooter class="w-full absolute bottom-0">
-          <button type="button" @click="closeModal" class="btn btn-outline-secondary w-20 mr-1">
-              Cancelar
-          </button>
-          <button type="button" @click="saveData" class="btn btn-primary w-20">
-              Editar
-          </button>
-      </ModalFooter>
+    <ModalHeader class="p-5">
+      <h2 class="font-medium text-base mr-auto">
+        {{ info.title }}
+      </h2>
+    </ModalHeader>
+    <ModalBody>
+      <div v-for="(field, index) in info.fields" :key="index">
+        <div v-if="field.edit != false">
+          <label :for="`modal-form-${index}`" class="form-label">{{
+            field.title
+          }}</label>
+          <template v-if="field.type === 'select'">
+            <select
+              :id="`modal-form-${index}`"
+              class="form-control mb-4"
+              v-model="form[field.model]"
+            >
+              <option
+                v-for="option in getOptions(field)"
+                :key="option.id"
+                :value="option.id"
+              >
+                {{ option.name }}
+              </option>
+            </select>
+          </template>
+          <template v-else>
+            <input
+              :id="`modal-form-${index}`"
+              :type="field.type"
+              class="form-control mb-4"
+              :placeholder="field.placeholder"
+              v-model="form[field.model]"
+              @keyup.enter="saveData"
+            />
+          </template>
+        </div>
+      </div>
+    </ModalBody>
+    <ModalFooter class="w-full absolute bottom-0">
+      <button
+        type="button"
+        @click="closeModal"
+        class="btn btn-outline-secondary w-20 mr-1"
+      >
+        Cancelar
+      </button>
+      <button type="button" @click="saveData" class="btn btn-primary w-20">
+        Editar
+      </button>
+    </ModalFooter>
   </Modal>
 </template>
 
 <script setup>
-import { ref, reactive, watchEffect } from 'vue';
-import api from '@/utils/api.js';
+import { ref, reactive, watchEffect } from "vue";
+import api from "@/utils/api.js";
 
 const props = defineProps({
   show: {
